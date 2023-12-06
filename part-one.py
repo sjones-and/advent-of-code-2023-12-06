2f280bb58d0eef342ea5c4fca13c22f92e638ae3
Stuart Jones <47328516+sjones-and@users.noreply.github.com>
2023-12-06 09:46:04 +0000

    Day six solution

--- part-one.py
+++ part-one.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 
 import os
+import math
 
 def answer(input_file):
     with open(input_file, "r") as input:
         data = input.read().split('\n')
+    times = [int(time) for time in data[0][10:].split(' ') if len(time) > 0]
+    distances = [int(distance) for distance in data[1][10:].split(' ') if len(distance) > 0]
+    answer = 1
+    for ix in range(len(times)):
+        sqrtbsqminusfourac = math.sqrt((times[ix] * times[ix]) - (4 * distances[ix]))
+        lower_limit = math.ceil((- times[ix] + sqrtbsqminusfourac) / -2)
+        upper_limit = math.floor((- times[ix] - sqrtbsqminusfourac) / -2)
+        if (lower_limit * times[ix]) - (lower_limit * lower_limit)  == distances[ix]:
+            lower_limit += 1
+        if (upper_limit * times[ix]) - (upper_limit * upper_limit)  == distances[ix]:
+            upper_limit -= 1
 
-    answer = None
+        answer = answer * (upper_limit - lower_limit + 1)
 
     print(f'The answer is: {answer}')
 
--- part-two.py
+++ part-two.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
 import os
+import math
 
 def answer(input_file):
     with open(input_file, "r") as input:
         data = input.read().split('\n')
+    time = int(data[0][10:].replace(' ',''))
+    distance = int(data[1][10:].replace(' ',''))
+    sqrtbsqminusfourac = math.sqrt((time * time) - (4 * distance))
+    lower_limit = math.ceil((- time + sqrtbsqminusfourac) / -2)
+    upper_limit = math.floor((- time - sqrtbsqminusfourac) / -2)
+    if (lower_limit * time) - (lower_limit * lower_limit)  == distance:
+        lower_limit += 1
+    if (upper_limit * time) - (upper_limit * upper_limit)  == distance:
+        upper_limit -= 1
 
-    answer = None
+    answer = (upper_limit - lower_limit + 1)
 
     print(f'The answer is: {answer}')
 
